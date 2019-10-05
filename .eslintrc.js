022698fc0518bc9f5a02f341d8806bd2a76a83a8
poberWong <poberwong@gmail.com>
2019-10-05 18:36:21 +0800

    add some global variables

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  globals: {
+    SCREEN_WIDTH: false,
+    SCREEN_HEIGHT: false,
+    iOS: false,
+    Android: false,
+    isIPhoneX: false,
+    jest: false,
+  },
 };
