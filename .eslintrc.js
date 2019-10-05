36abdb009feb7ac4ffd49d6c44facea96ce3e00a
poberWong <poberwong@gmail.com>
2019-10-05 21:07:49 +0800

    fix eslint error about babel-decorators and open inline styles

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,16 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  parserOptions: {
+    ecmaFeatures: {
+      legacyDecorators: true,
+    },
+  },
+  rules: {
+    'react-native/no-inline-styles': 0,
+  },
   globals: {
+    AppVersion: false,
     SCREEN_WIDTH: false,
     SCREEN_HEIGHT: false,
     iOS: false,
