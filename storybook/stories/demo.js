ff7c96f43d0a8f417becd2c96a17d34d975b5199
poberWong <poberwong@gmail.com>
2019-10-11 05:39:28 +0800

    remove useless story

--- storybook/stories/demo.js
+++ storybook/stories/demo.js
@@ -3,15 +3,9 @@ import { Text } from 'react-native';
 
 import { storiesOf } from '@storybook/react-native';
 import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 
 import Button from './Button';
 import CenterView from './CenterView';
-import Welcome from './Welcome';
-
-storiesOf('Welcome', module).add('to Storybook', () => (
-  <Welcome showApp={linkTo('Button')} />
-));
 
 storiesOf('Button', module)
   .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
